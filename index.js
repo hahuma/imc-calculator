1e2f26a26292987d930ac84309bd24af8c7ea518
Petterson <58094725+hahuma@users.noreply.github.com>
2020-11-08 05:00:47 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const getElement = document.querySelector.bind(document)
+
+const buttons = {
+    report: getElement('#report-button'),
+    aboutImc: getElement('#about-imc-button'),
+    contactUs: getElement('#contact-us-button'),
+}
+
+const divs = {
+    report: getElement('#report-div'),
+    aboutImc: getElement('#about-imc-div'),
+    contactUs: getElement('#contact-us-div'),
+
+}
