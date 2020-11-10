4407fd02f8154595290784f24f82e11a24ff9111
Petterson <58094725+hahuma@users.noreply.github.com>
2020-11-10 01:08:37 -0200

    added javascript implementation

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
-const getElement = document.querySelector.bind(document)
+import ManifestJSON from './manifest.js'
 
-const buttons = {
-    report: getElement('#report-button'),
-    aboutImc: getElement('#about-imc-button'),
-    contactUs: getElement('#contact-us-button'),
+const getElement = document.querySelector.bind(document)
+const IMCsInfo = {}
+
+const form = getElement('#calculate-imc-form')
+
+ManifestJSON.forEach( function(index) {
+    return IMCsInfo[index.imcLevel] = {
+        minValue: index.minValue,
+        maxValue: index.maxValue,
+        text: index.text
+    }
+})
+Object.freeze(IMCsInfo)
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const weight = getElement('#weight').value
+    const height = getElement('#height').value
+    const {
+        formattedHeight,
+        formattedWeight
+    } = validadeInputValues(weight, height)
+
+    const imc = (formattedWeight / (formattedHeight * formattedHeight / 10000)).toFixed(1)
+
+    const textResult = isBetween(imc, IMCsInfo)
+
+    return alert(textResult)
+})
+
+
+function isBetween(value, IMCsInfo){
+
+    if ( IMCsInfo.low.minValue <= value && value <= IMCsInfo.low.maxValue ){
+        return IMCsInfo.low.text
+    }
+    if ( IMCsInfo.normal.minValue <= value && value <= IMCsInfo.normal.maxValue ){
+        return IMCsInfo.normal.text
+    }
+    if ( IMCsInfo.overweight.minValue <= value && value <= IMCsInfo.overweight.maxValue ){
+        return IMCsInfo.overweight.text
+    }
+    if ( IMCsInfo.obese.minValue <= value && value <= IMCsInfo.obese.maxValue ){
+        return IMCsInfo.obese.text
+    }
+    if ( IMCsInfo.morbid.minValue <= value && value <= IMCsInfo.morbid.maxValue ){
+        return IMCsInfo.morbid.text
+    }
 }
 
-const divs = {
-    report: getElement('#report-div'),
-    aboutImc: getElement('#about-imc-div'),
-    contactUs: getElement('#contact-us-div'),
 
+function validadeInputValues(weight = "", height = "") {
+    const formattedWeight = Number(weight.replace(',', '.')).toFixed(2)
+    const formattedHeight = Number(height).toFixed(2)
+    return {
+        formattedWeight,
+        formattedHeight
+
+    }
 }
---
+++ manifest.js
@@ -0,0 +1,32 @@
+export default[
+    {
+        "imcLevel": "low",
+        "minValue": 0,
+        "maxValue": 18.4,
+        "text": "Seu IMC está abaixo do ideal para a sua altura e peso."
+    },
+    {
+        "imcLevel": "normal",
+        "minValue": 18.5,
+        "maxValue": 24.9,
+        "text": "Seu IMC está no nível ideal para a sua altura e peso."
+    },
+    {
+        "imcLevel": "overweight",
+        "minValue": 25,
+        "maxValue": 29.9,
+        "text": "Seu IMC está acima do ideal para a sua altura e peso classificado como sobrepeso."
+    },
+    {
+        "imcLevel": "obese",
+        "minValue": 30,
+        "maxValue": 39.9,
+        "text": "Seu IMC está muito acima do ideal para a sua altura e peso classificado como obesidade."
+    },
+    {
+        "imcLevel": "morbid",
+        "minValue": 40,
+        "maxValue": 999,
+        "text": "Seu IMC está extremamente acima do ideal para a sua altura e peso classificado como obesidade mórbida."
+    }
+]
